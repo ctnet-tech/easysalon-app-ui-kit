f1f5a297452ff726ce08706b8b2b8dc87fb70599
nguyenphuochai1998 <haito1998@gmail.com>
2021-07-03 08:26:36 +0700

    Task-AQL4
    Sửa Widget Button theo style ui app

--- lib/configs/themes.dart
+++ lib/configs/themes.dart
@@ -1,23 +1,36 @@
 import 'package:easysalon_mobile_ui_kit/services/theme_notifier.dart';
+import 'package:flutter/cupertino.dart';
 
 var lightTheme = Theme(
     darkest: Theme.colorFromHex("#000000"),
     dark: Theme.colorFromHex("#222B45"),
     primary: Theme.colorFromHex("#6249E3"),
     secondary: Theme.colorFromHex("#8F9BB3"),
-    danger: Theme.colorFromHex("#FF006E"),
+    danger: Theme.colorFromHex("#FF2D55"),
     weak: Theme.colorFromHex("#F2F4F6"),
     warning: Theme.colorFromHex("#FCAD62"),
     light: Theme.colorFromHex("#F9F9F9"),
-    lightest: Theme.colorFromHex("#FFFFFF"));
+    lightest: Theme.colorFromHex("#FFFFFF"),
+    bondiBlue: Theme.colorFromHex("#1A9AFF"),
+    pastelGreen: Theme.colorFromHex("#5FDC65"),
+    darkOrange: Theme.colorFromHex("#FF8900"),
+    radicalRed: Theme.colorFromHex("#FF2D55"),
+    lightSlateBlue: Theme.colorFromHex("#884DFF"),
+);
 
 var darkTheme = Theme(
-    darkest: Theme.colorFromHex("#000"),
-    dark: Theme.colorFromHex("#000"),
-    primary: Theme.colorFromHex("#000"),
-    secondary: Theme.colorFromHex("#000"),
-    danger: Theme.colorFromHex("#000"),
-    weak: Theme.colorFromHex("#000"),
-    warning: Theme.colorFromHex("#000"),
-    light: Theme.colorFromHex("#000"),
-    lightest: Theme.colorFromHex("#000"));
+  darkest: Theme.colorFromHex("#000"),
+  dark: Theme.colorFromHex("#000"),
+  primary: Theme.colorFromHex("#000"),
+  secondary: Theme.colorFromHex("#000"),
+  danger: Theme.colorFromHex("#000"),
+  weak: Theme.colorFromHex("#000"),
+  warning: Theme.colorFromHex("#000"),
+  light: Theme.colorFromHex("#000"),
+  lightest: Theme.colorFromHex("#000"),
+  bondiBlue: Theme.colorFromHex("#000"),
+  pastelGreen: Theme.colorFromHex("#5FDC65"),
+  darkOrange: Theme.colorFromHex("#FF8900"),
+  radicalRed: Theme.colorFromHex("#FF8900"),
+  lightSlateBlue: Theme.colorFromHex("#884DFF"),
+);
