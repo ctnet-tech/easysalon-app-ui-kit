ff03646f64378c5069fd9a4d8cd371d5383c3e25
quangkhang20899 <quangkhang20899@gmail.com>
2021-07-12 08:53:08 +0700

    chore: format after merge

--- lib/pages/home/widgets_home_page.dart
+++ lib/pages/home/widgets_home_page.dart
@@ -1,19 +1,32 @@
+import 'package:easysalon_mobile_ui_kit/bloc/date_picker_bloc/date_picker_blocs.dart';
+import 'package:easysalon_mobile_ui_kit/bloc/date_range_picker_bloc/date_range_picker_blocs.dart';
 import 'package:easysalon_mobile_ui_kit/configs/icons/line_icons.dart';
 import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/button_page.dart';
 import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/icon_page.dart';
 import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/paragraph_page.dart';
+import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/report_panel_page.dart';
 import 'package:easysalon_mobile_ui_kit/services/theme_notifier.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/CustomSlidable/BaseWidget/actions.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/CustomSlidable/custom_slidable.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/date_picker.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/expandable_button.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/basic/panel.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/profile_admin.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/selection_time_bar.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/icons/CustomIcon.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/layout/page_header.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/layout/space.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/layout/standard_page.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/navigation/menu/menu.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/navigation/menu/menu_item.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/typography/paragraph.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WidgetsHomePage extends StatefulWidget {
   WidgetsHomePage() : super(key: Key("HOME"));
+
   @override
   _WidgetsHomePageState createState() => _WidgetsHomePageState();
 }
@@ -76,11 +89,12 @@ class _WidgetsHomePageState extends State<WidgetsHomePage> {
                           padding: EdgeInsets.all(12),
                           width: MediaQuery.of(context).size.width,
                           child: Text("ádsađấhdjsáđâsđâsđâsáđâsd"))),
-                  ),
                   BlocProvider(
                     create: (_) => DateRangePickerBloc(),
                     child: SelectionTimeBar(),
                   ),
+                  SizedBox(height: 20,),
+                  Paragraph(content: "DatePicker",),
                   BlocProvider(
                     create: (_) => DatePickerBloc(),
                     child: DatePicker(
