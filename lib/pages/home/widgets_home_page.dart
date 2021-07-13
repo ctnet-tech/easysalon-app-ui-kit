e2edc46737e1a7395dc6611eb71b8de286816a42
quangkhang20899 <quangkhang20899@gmail.com>
2021-07-13 14:11:14 +0700

    fix: thêm widget test

--- lib/pages/home/widgets_home_page.dart
+++ lib/pages/home/widgets_home_page.dart
@@ -3,6 +3,8 @@ import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/button_page.dart';
 import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/icon_page.dart';
 import 'package:easysalon_mobile_ui_kit/pages/widgets/generic/paragraph_page.dart';
 import 'package:easysalon_mobile_ui_kit/services/theme_notifier.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/CustomSlidable/BaseWidget/actions.dart';
+import 'package:easysalon_mobile_ui_kit/widgets/basic/CustomSlidable/custom_slidable.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/basic/panel.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/icons/CustomIcon.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/layout/page_header.dart';
@@ -10,6 +12,7 @@ import 'package:easysalon_mobile_ui_kit/widgets/layout/space.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/layout/standard_page.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/navigation/menu/menu.dart';
 import 'package:easysalon_mobile_ui_kit/widgets/navigation/menu/menu_item.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class WidgetsHomePage extends StatefulWidget {
@@ -36,7 +39,37 @@ class _WidgetsHomePageState extends State<WidgetsHomePage> {
                       MenuItem(label: "Button", to: ButtonPage.path),
                       MenuItem(label: "Icon", to: IconPage.path)
                     ],
-                  )
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomSlidable(
+                      actions: [
+                        SlidableAction(
+                          onPressed: (_) {
+                            print(12);
+                          },
+                          icon: LineIcons.edit_square_feather,
+                          label: "Sửa",
+                          backgroundColor: Colors.blue,
+                          flex: 1,
+//                          spacing: 20,
+                        ),
+                        SlidableAction(
+                          onPressed: (_) {
+                            print(12);
+                          },
+                          icon: LineIcons.trash,
+                          label: "Xóa",
+                          backgroundColor: Colors.green,
+                          flex: 1,
+//                          spacing: 20,
+                        ),
+                      ],
+                      child: Container(
+                          padding: EdgeInsets.all(12),
+                          width: MediaQuery.of(context).size.width,
+                          child: Text("ádsađấhdjsáđâsđâsđâsáđâsd"))),
                 ],
               )))
     ]);
