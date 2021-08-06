84501b73ec123188c6807c10729a2a64fbad3ddf
quangkhang20899 <quangkhang20899@gmail.com>
2021-08-06 11:29:15 +0700

    fix: add notifyListener() for provider

--- lib/provider/customer_services_provider/customer_services_provider.dart
+++ lib/provider/customer_services_provider/customer_services_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CustomerServicesProvider extends ChangeNotifier {
+class CustomerServicesProvider extends ChangeNotifier
+{
   List<List<List<String?>>> listCustomerService = [];
   List<List<Map<String, Map<String, List<int>>>>> listCustomerSubService = [];
   List<List<String?>> dropdownSubServiceValue = [];
@@ -8,45 +9,52 @@ class CustomerServicesProvider extends ChangeNotifier {
   List<String?> notes = [];
   List<List<List<int>>> timeAndCostSubService = [];
 
-  addServiceGroup({required int customerIndex}) {
+  addServiceGroup({required int customerIndex})
+  {
     notifyListeners();
   }
 
-  addCustomer() {
+  addCustomer()
+  {
     listCustomerService.add([]);
     notes.add(null);
     notifyListeners();
   }
 
-  removeCustomer({required int customerIndex}) {
+  removeCustomer({required int customerIndex})
+  {
     listCustomerService.removeAt(customerIndex);
     listCustomerSubService.removeAt(customerIndex);
     timeAndCostSubService.removeAt(customerIndex);
     notes.removeAt(customerIndex);
+    notifyListeners();
   }
 
   removeServiceGroup({
     required int customerIndex,
     required int serviceIndex,
-  }) {
+  })
+  {
     for (int i = serviceIndex;
-        i < listCustomerService[customerIndex].length - 1;
-        i++) {
+    i < listCustomerService[customerIndex].length - 1;
+    i++)
+    {
       listCustomerService[customerIndex][i] =
-          listCustomerService[customerIndex][i + 1];
+      listCustomerService[customerIndex][i + 1];
       listCustomerSubService[customerIndex][i] =
-          listCustomerSubService[customerIndex][i + 1];
+      listCustomerSubService[customerIndex][i + 1];
       dropdownSubServiceValue[customerIndex][i] =
-          dropdownSubServiceValue[customerIndex][i + 1];
+      dropdownSubServiceValue[customerIndex][i + 1];
       dropdownServiceGroupValue[customerIndex][i] =
-          dropdownServiceGroupValue[customerIndex][i + 1];
+      dropdownServiceGroupValue[customerIndex][i + 1];
       timeAndCostSubService[customerIndex][i] =
-          timeAndCostSubService[customerIndex][i + 1];
+      timeAndCostSubService[customerIndex][i + 1];
     }
     listCustomerService[customerIndex].removeLast();
     listCustomerSubService[customerIndex].removeLast();
     dropdownSubServiceValue[customerIndex].removeLast();
     dropdownServiceGroupValue[customerIndex].removeLast();
     timeAndCostSubService[customerIndex].removeLast();
+    notifyListeners();
   }
 }
